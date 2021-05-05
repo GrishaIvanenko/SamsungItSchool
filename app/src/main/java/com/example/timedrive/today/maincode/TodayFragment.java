6c2bb721dc3d89a0e80dd91e2216799558e414c6
GrishaIvanenko <ivanenko_ga_0918@1511.ru>
2021-05-05 14:00:47 +0300

    добавлен recyclerview в TodayFragment

--- app/src/main/java/com/example/timedrive/today/maincode/TodayFragment.java
+++ app/src/main/java/com/example/timedrive/today/maincode/TodayFragment.java
@@ -7,14 +7,46 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.timedrive.R;
+import com.example.timedrive.database.asks.AsyncAll;
+import com.example.timedrive.database.code.Task;
+import com.example.timedrive.database.code.TaskBase;
+import com.example.timedrive.today.recyclerview.recyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class TodayFragment extends Fragment {
+    private TaskBase db;
+    private RecyclerView recyclerView;
+    private ArrayList<Task> cash;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_today, container, false);
+        AsyncAll getter = new AsyncAll(this.getContext());
+        getter.execute();
+        try {
+            cash = getter.get();
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
+        recyclerView = root.findViewById(R.id.recyclerViewInToday);
+        setAdapter();
         return root;
     }
+
+    private void setAdapter() {
+        recyclerAdapter adapter = new recyclerAdapter(cash);
+        RecyclerView.LayoutManager layoutManager =
+                new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(adapter);
+    }
 }
+
